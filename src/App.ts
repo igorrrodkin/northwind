9e6fb5fba74c5703e20642f94826ebeefd75b354
Igor Rodkin <igorrodkin@Igors-MacBook-Air.local>
2023-02-01 22:04:28 +0200

    prod

--- src/App.ts
+++ src/App.ts
@@ -14,7 +14,7 @@ class App {
   }
   public listen = () => {
     this.app.listen(this.port, () => {
-      console.log(`App listening on the port ${this.port}`);
+      console.log(`App is listening on the port ${this.port}`);
     });
   };
   private initializeMiddlewares = () => {
